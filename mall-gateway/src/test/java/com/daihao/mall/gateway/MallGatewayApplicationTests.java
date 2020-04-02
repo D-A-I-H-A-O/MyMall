b729697533d16270a2207ca4aa1d3c493e885332
DAIHAO <651433368@qq.com>
2020-04-02 22:20:30 +0800

    项目初始化

---
+++ mall-gateway/src/test/java/com/daihao/mall/gateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daihao.mall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
